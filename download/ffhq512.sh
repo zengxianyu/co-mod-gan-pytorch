5b1ab4270ab0291405ecfb77f3835ea2581e93eb
zengxianyu <zengxianyu18@gmail.com>
2021-06-22 00:08:15 +0800

    scropt download

---
+++ download/ffhq512.sh
@@ -0,0 +1,2 @@
+wget https://maildluteducn-my.sharepoint.com/:u:/g/personal/zengyu_mail_dlut_edu_cn/Ee1YPJG2Y7NDnUjJBf-SipoBBSlbv8QfFy6K7lsiiiiFHg?download=1
+mv Ee1YPJG2Y7NDnUjJBf-SipoBBSlbv8QfFy6K7lsiiiiFHg?download=1 ./checkpoints/co-mod-gan-ffhq-9-025000.pth
