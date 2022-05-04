b647f453db250d62afd3afe572d190ee6aaec551
zengxianyu <zengxianyu18@gmail.com>
2022-05-04 18:03:28 -0400

    attpool

--- models/networks/co_mod_gan.py
+++ models/networks/co_mod_gan.py
@@ -139,6 +139,43 @@ class G_synthesis_co_mod_gan(nn.Module):
                 x = self.Dense0(x)
                 x = self.dropout(x)
                 return x, E_features, x
+        class AttentionPool2d(nn.Module):
+            def __init__(self, spacial_dim: int, embed_dim: int, num_heads: int, output_dim: int = None):
+                super().__init__()
+                self.spacial_dim = spacial_dim
+                self.positional_embedding = nn.Parameter(torch.randn(spacial_dim ** 2 + 1, embed_dim) / embed_dim ** 0.5)
+                self.k_proj = nn.Linear(embed_dim, embed_dim)
+                self.q_proj = nn.Linear(embed_dim, embed_dim)
+                self.v_proj = nn.Linear(embed_dim, embed_dim)
+                self.c_proj = nn.Linear(embed_dim, output_dim or embed_dim)
+                self.num_heads = num_heads
+
+            def forward(self, x):
+                x = F.interpolate(x, (self.spacial_dim, self.spacial_dim))
+                x = x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3]).permute(2, 0, 1)  # NCHW -> (HW)NC
+                x = torch.cat([x.mean(dim=0, keepdim=True), x], dim=0)  # (HW+1)NC
+                x = x + self.positional_embedding[:, None, :].to(x.dtype)  # (HW+1)NC
+                x, _ = F.multi_head_attention_forward(
+                    query=x, key=x, value=x,
+                    embed_dim_to_check=x.shape[-1],
+                    num_heads=self.num_heads,
+                    q_proj_weight=self.q_proj.weight,
+                    k_proj_weight=self.k_proj.weight,
+                    v_proj_weight=self.v_proj.weight,
+                    in_proj_weight=None,
+                    in_proj_bias=torch.cat([self.q_proj.bias, self.k_proj.bias, self.v_proj.bias]),
+                    bias_k=None,
+                    bias_v=None,
+                    add_zero_attn=False,
+                    dropout_p=0,
+                    out_proj_weight=self.c_proj.weight,
+                    out_proj_bias=self.c_proj.bias,
+                    use_separate_proj_weight=True,
+                    training=self.training,
+                    need_weights=False
+                )
+
+                return x[0]
         class E_block_final_conv(nn.Module): # res = 2..resolution_log2
             def __init__(self):
                 super().__init__()
@@ -155,12 +192,18 @@ class G_synthesis_co_mod_gan(nn.Module):
                 self.Dense0 = EqualLinear(nf(1)*2, nf(1)*2,
                                 activation="fused_lrelu")
                 self.dropout = nn.Dropout(opt.dropout_rate)
+                if opt.att_pool:
+                    self.att_pool = AttentionPool2d(spacial_dim=8,
+                            embed_dim=nf(1)*2, num_heads=32, output_dim=nf(1)*2)
             def forward(self, data):
                 x, E_features = data
                 x = self.Conv(x)
                 E_features[2] = x
                 x_feat = self.ConvDense0(x)
-                x = F.adaptive_avg_pool2d(x_feat, 1)
+                if opt.att_pool:
+                    x = self.att_pool(x_feat)
+                else:
+                    x = F.adaptive_avg_pool2d(x_feat, 1)
                 bsize = x.size(0)
                 x = x.view(bsize, -1)
                 x = self.Dense0(x)
@@ -367,6 +410,7 @@ class Generator(BaseNetwork):
         parser.add_argument('--style_mod',         type=bool, default= True,)
         parser.add_argument('--noise_injection',   type=bool, default= True,)
         parser.add_argument('--conv_enc', action='store_true')
+        parser.add_argument('--att_pool', action='store_true')
         return parser
     def __init__(
             self,
---
+++ scripts_train/train_vgg_l1_conv.sh
@@ -0,0 +1,26 @@
+export CXX="g++"
+python train.py \
+	--conv_enc \
+	--batchSize $BSIZE \
+	--nThreads $NWK \
+	--name comod_vgg_l1_conv \
+	--train_image_dir ./datasets/places/places2 \
+	--train_image_list ./datasets/places/places2_standard_challenge.txt \
+	--train_image_postfix '' \
+	--val_image_dir ./datasets/places2sample1k_val/places2samples1k_crop256 \
+	--val_image_list ./datasets/places2sample1k_val/files.txt \
+	--val_mask_dir ./datasets/places2sample1k_val/places2samples1k_256_mask_square128 \
+	--load_size 512 \
+	--crop_size 256 \
+	--z_dim 512 \
+	--validation_freq 10000 \
+	--niter 50 \
+	--dataset_mode trainimage \
+	--trainer stylegan2 \
+	--dataset_mode_train trainimage \
+	--dataset_mode_val valimage \
+	--model comod \
+	--netG comodgan \
+	--netD comodgan \
+	--preprocess_mode scale_shortside_and_crop \
+	$EXTRA
